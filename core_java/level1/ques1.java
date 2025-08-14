1d6f459fa37ec8dd4920163e0fce5270a991311f
Ayush Thakur <ayushthakur@Ayushs-MacBook-Air-3.local>
2025-08-14 15:35:55 +0530

    first question

---
+++ core_java/level1/ques1.java
@@ -0,0 +1,7 @@
+public class ques1 {
+    public static void main(String[] args) {
+        int birthage = 2000;
+        int currentyear= 2024;
+        System.out.println("Harry's age in 2024 is "+(currentyear-birthage));
+    }
+}
