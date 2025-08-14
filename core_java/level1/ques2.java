c3482be0c392501d3a8d74bc78a2d96d3622ed07
Ayush Thakur <ayushthakur@Ayushs-MacBook-Air-3.local>
2025-08-14 15:52:56 +0530

    second commit

---
+++ core_java/level1/ques2.java
@@ -0,0 +1,9 @@
+public class ques2 {
+    public static void main(String[] args) {
+        int m = 94;
+        int p = 95;
+        int c = 96;
+        int avg = (m+p+c)/3;
+        System.out.println("Sam’s average mark in PCM is "+ avg);
+    }
+}
---
+++ core_java/level1/ques3.java
@@ -0,0 +1,8 @@
+public class ques3 {
+    public static void main(String[] args) {
+       double a= 10.8;
+       double b=0.6;
+        double c= a*b;
+        System.out.println("The distance "+ a +" km in miles is "+ c);
+    }
+}
